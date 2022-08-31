9573e5ffd9eb098cae962aa52ccd8cb258bbf44d
Yousef-Hatem <yousef26hatem@gmail.com>
2022-08-31 02:27:11 +0200

    Replace Fetch With Axios

--- src/tradingview.ts
+++ src/tradingview.ts
@@ -1,28 +1,29 @@
-import fetch from 'node-fetch';
+import axios from "axios";
 import { Idea } from './interfaces/Ideas';
 
 export default class Tradingview {
 
-    async dataHandler(data: Promise<string>): Promise<Idea[]> {
+    async dataHandler(HTML: string): Promise<Idea[]> {
         let ideas: any[] = [];
-        await data.then(async HTML => {
-            let items: string[] = HTML.split('tv-feed__item tv-feed-layout__card-item');
-            items.shift();
-    
-            items.forEach(async item => {
-                if (ideas.length < 5) {
-                    let idea = {
-                        title: item.split('tv-widget-idea__title apply-overflow-tooltip js-widget-idea__popup" data-href="')[1].split("</a>")[0].split('">')[1],
-                        symbol: item.split('class="tv-widget-idea__symbol apply-overflow-tooltip">')[1].split('</a></div>')[0],
-                        username: item.split('<span class="tv-card-user-info__name">')[1].split('</span>')[0],
-                        badgeWrap: (item.split('<span class="content-s1XFg_zx">')[1] || '').split('</span>')[0],
-                        img: item.split('type="image/webp"><img data-src="')[1].split('" src="')[0],
-                        time: item.split(`publishedUrl&#34;:&#34;/chart/`)[1].split('/&#34;')[0]
-                    };
-            
-                    ideas.push(idea);
-                }
-            });
+
+        let items: string[] = HTML.split('tv-feed__item tv-feed-layout__card-item');
+
+        items.shift();
+
+        items.forEach(async item => {
+            if (ideas.length < 5) {
+                let idea = {
+                    title: item.split('tv-widget-idea__title apply-overflow-tooltip js-widget-idea__popup" data-href="')[1].split("</a>")[0].split('">')[1],
+                    symbol: item.split('class="tv-widget-idea__symbol apply-overflow-tooltip">')[1].split('</a></div>')[0],
+                    username: item.split('<span class="tv-card-user-info__name">')[1].split('</span>')[0],
+                    badgeWrap: (item.split('<span class="content-s1XFg_zx">')[1] || '').split('</span>')[0],
+                    img: item.split('type="image/webp"><img data-src="')[1].split('" src="')[0],
+                    url: item.split(`publishedUrl&#34;:&#34;/chart/`)[1].split('/&#34;')[0],
+                    time: item.split(`publishedUrl&#34;:&#34;/chart/`)[1].split('/&#34;')[0]
+                };
+        
+                ideas.push(idea);
+            }
         });
 
         for (let i = 0; i < ideas.length; i++) {
@@ -37,10 +38,9 @@ export default class Tradingview {
     async dateHandler(route: string): Promise<Date> {
         let url = 'https://www.tradingview.com/chart/' + route;
         let date: Date;
-        const response: fetch = await fetch(url);
-        
-        return await response.text().then(html => { 
-            date = new Date(html.split('"datePublished": "')[1].split('",')[0]);
+
+        return await axios.get(url).then((req) => {
+            date = new Date(req.data.split('"datePublished": "')[1].split('",')[0]);
             date.setHours(date.getHours() + 7);            
             return date;
         });
@@ -49,21 +49,25 @@ export default class Tradingview {
     async getIdeas(symbol: string): Promise<Idea[] | null> {
         let url = 'https://www.tradingview.com/ideas/' + symbol;
 
-        const response: fetch = await fetch(url);
         let ideas: Idea[] = [];
-        await this.dataHandler(response.text()).then(data => {
-            ideas = data;
+
+        await axios.get(url).then(async req => {
+            await this.dataHandler(req.data).then(data => {
+                ideas = data;
+            });
         });
 
         if (!ideas.length) {
             return null;
         }
         
-        const responseRecent: fetch = await fetch(url + "?sort=recent");        
         let ideasRecent: Idea[] = [];
-        await this.dataHandler(responseRecent.text()).then(data => {            
-            ideasRecent = data;
-        });
+
+        await axios.get(url + "?sort=recent").then(async req => {
+            await this.dataHandler(req.data).then(data => {            
+                ideasRecent = data;
+            });
+        });        
         
         return [...ideas, ...ideasRecent];
     }
